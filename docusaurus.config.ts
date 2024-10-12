f2fbfa68cfe6b3e6fb100f6b5f8e6d401c4f8e48
Connor Sanderson <65951311+ConnorSand@users.noreply.github.com>
2024-10-12 21:35:54 +1100

    change baseUrl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   url: 'https://conops.io', // Change this to your actual domain
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/connor-docusaurus/',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
